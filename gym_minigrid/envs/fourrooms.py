988e29bc3dc4e0aef452fea04f54fd46f413196a
Andi Peng <andipeng@mit.edu>
2021-05-11 12:08:00 -0400

    fourroomsfixed

--- gym_minigrid/envs/fourrooms.py
+++ gym_minigrid/envs/fourrooms.py
@@ -125,6 +125,58 @@ class FourRoomsEnvFixed(FourRoomsEnv):
 
         self.mission = 'Reach the goal'
 
+class FourRoomsEnvFixedClose(FourRoomsEnv):
+    def __init__(self):
+        super().__init__(
+            agent_pos=(5,14),
+            goal_pos=(13, 13)
+        )
+    
+    def _gen_grid(self, width, height):
+        # Create the grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height - 1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
+
+        room_w = width // 2
+        room_h = height // 2
+
+        # For each row of rooms
+        for j in range(0, 2):
+
+            # For each column
+            for i in range(0, 2):
+                xL = i * room_w
+                yT = j * room_h
+                xR = xL + room_w
+                yB = yT + room_h
+
+                # Bottom wall and door
+                if i + 1 < 2:
+                    self.grid.vert_wall(xR, yT, room_h)
+                    pos = (xR, int((yT + 1 + yB)/2))
+                    self.grid.set(*pos, None)
+
+                # Bottom wall and door
+                if j + 1 < 2:
+                    self.grid.horz_wall(xL, yB, room_w)
+                    pos = (int((xL + 1 + xR)/2), yB)
+                    self.grid.set(*pos, None)
+
+        self.agent_pos = self._agent_default_pos
+        self.grid.set(*self._agent_default_pos, None)
+        self.agent_dir = 0
+
+        goal = Goal()
+        self.put_obj(goal, *self._goal_default_pos)
+        goal.init_pos, goal.cur_pos = self._goal_default_pos
+
+        self.mission = 'Reach the goal'
+
 register(
     id='MiniGrid-FourRooms-v0',
     entry_point='gym_minigrid.envs:FourRoomsEnv'
@@ -133,4 +185,9 @@ register(
 register(
     id='MiniGrid-FourRoomsFixed-v0',
     entry_point='gym_minigrid.envs:FourRoomsEnvFixed'
+)
+
+register(
+    id='MiniGrid-FourRoomsFixedClose-v0',
+    entry_point='gym_minigrid.envs:FourRoomsEnvFixedClose'
 )
